ddb4ab4d7ae0d0f30db40a8d89b3393dc8c9d667
vivgoo <mjsong01@163.com>
2020-09-29 00:48:11 +0800

    Completed Ring Queue Data Structures

--- ringqueue.go
+++ ringqueue.go
@@ -1,9 +1,64 @@
 package gdsutil
 
-type RingQueue struct {
+type STATUS int
 
+const (
+	SUCCESS      STATUS = 1
+	FAIL      STATUS = 0
+)
+
+type RingQueue struct {
+	queueData []interface{}
+	maxSize int
+	head int
+	tail int
+}
+
+func InitRingQueue(maxSize int) (rq RingQueue, status STATUS) {
+	if maxSize <= 0  {return RingQueue{},FAIL}
+	return RingQueue{
+		queueData: make([]interface{},maxSize),
+		maxSize: maxSize,
+		head: maxSize - 1,
+		tail: maxSize - 1,
+	},SUCCESS
+
+	return RingQueue{},FAIL
 }
 
 func (rq *RingQueue) IsEmpty() bool  {
-	return false
+	return rq.head == rq.tail
+}
+
+func (rq *RingQueue) IsFull() bool  {
+	return (rq.tail + 1) % rq.maxSize == rq.head
+}
+
+func (rq *RingQueue) EnQueue(data interface{})  STATUS {
+	if (rq.tail + 1) % rq.maxSize == rq.head {
+		return FAIL
+	}
+	rq.tail = (rq.tail + 1) % rq.maxSize
+	rq.queueData[rq.tail] = data
+
+	return SUCCESS
+}
+
+func (rq *RingQueue) HeadData() (data interface{},status STATUS)  {
+	var tempData interface{}
+	if rq.head == rq.tail {
+		return tempData,FAIL
+	}
+	return rq.queueData[(rq.head+1) % rq.maxSize],SUCCESS
+}
+
+func (rq *RingQueue) DeQueue() (data interface{},status STATUS) {
+	var tempData interface{}
+	if rq.head == rq.tail {
+		return tempData,FAIL
+	}
+
+	rq.head = (rq.head + 1) % rq.maxSize
+	tempData = rq.queueData[rq.head]
+	return tempData,SUCCESS
 }
--- ringqueue_test.go
+++ ringqueue_test.go
@@ -1,1 +1,76 @@
 package gdsutil
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInitRingQueue(t *testing.T) {
+	_,err := InitRingQueue(0)
+	if err == SUCCESS {
+		t.Error("fail")
+	}
+
+	_,err = InitRingQueue(5)
+	if err == FAIL{
+		t.Error("fail")
+	}
+}
+
+type person struct {
+	name string
+}
+func TestRingQueue_EnQueue(t *testing.T) {
+	q,_ := InitRingQueue(5)
+	q.EnQueue(1)
+	q.EnQueue(2)
+	q.EnQueue(3)
+	q.EnQueue(4)
+
+	if q.EnQueue(5) == SUCCESS {
+		t.Error("队列已满")
+	}
+
+}
+
+
+
+func TestRingQueue_HeadData(t *testing.T) {
+	q,_ := InitRingQueue(5)
+	q.EnQueue(1)
+	q.EnQueue(2)
+	q.EnQueue(3)
+	q.EnQueue(4)
+
+	d,_ := q.HeadData()
+	if d != 1 {
+		t.Error("应该为1")
+	}
+}
+
+func TestRingQueue_DeQueue(t *testing.T) {
+	q,_ := InitRingQueue(5)
+	p1 := person{"James soong"}
+	q.EnQueue(p1)
+	p2 := person{"Jin Wang"}
+	q.EnQueue(p2)
+	p3 := person{"Li Jian"}
+	q.EnQueue(p3)
+	p4 := person{"Xia Wan"}
+	q.EnQueue(p4)
+
+	p5 := person{"ChunYan Shen"}
+	q.EnQueue(p5)
+
+	for q.IsEmpty() == false {
+		t,_ := q.DeQueue()
+		fmt.Println(t)
+	}
+}
+
+func TestRingQueue_IsEmpty(t *testing.T) {
+	q,_ := InitRingQueue(5)
+	if q.IsEmpty() == false {
+		t.Error("应该是空")
+	}
+}
